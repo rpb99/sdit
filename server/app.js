800481abcb2e00074e5e75efcdbf0c19c9fe69b0
rpb99 <rikipebrianto@gmail.com>
2021-03-24 06:58:21 +0700

    init commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const { readdirSync } = require("fs");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+
+module.exports = app;
